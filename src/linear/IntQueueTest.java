81f4ec1b076b656a6db9c71b1673415b1fc615a2
Vangelis <bulanla@outlook.com>
2020-12-17 16:41:57 +0800

    feat: 环形队列实现

--- src/linear/IntQueueTest.java
+++ src/linear/IntQueueTest.java
@@ -12,12 +12,14 @@ public class IntQueueTest {
         queue.add(1);
         queue.add(2);
         queue.add(3);
-        System.out.println(queue);
+        queue.show();
         System.out.println(queue.get());
-        System.out.println(queue);
+        System.out.println(queue.get());
+        System.out.println(queue.get());
+
+        queue.show();
 
 
-        System.out.println(4%3);
     }
 
 
@@ -26,7 +28,7 @@ public class IntQueueTest {
         /**
          * 存的数组
          */
-        private Integer[] array;
+        private final Integer[] array;
         /**
          * 数组大小
          */
@@ -34,29 +36,32 @@ public class IntQueueTest {
         /**
          * 数组大小
          */
-        private final int DEFAUT_MAX_SIZE=3;
+        private final int DEFAUT_MAX_SIZE = 3;
 
         /**
          * 取的下标
          */
-        private int front = -1;
+        private int front;
 
         /**
          * 读的下标
          */
-        private int rear = -1;
+        private int rear;
 
         /**
          * 构造，初始化
+         *
          * @param maxSize 数组大小
          */
         public IntQueue(int maxSize) {
             if (maxSize < 1) {
                 throw new IllegalArgumentException("maxSize 值非法");
             }
-            this.maxSize = maxSize;
-            this.array = new Integer[maxSize];
+            this.maxSize = maxSize+1;
+
+            this.array = new Integer[maxSize+1];
         }
+
         private IntQueue() {
             this.maxSize = DEFAUT_MAX_SIZE;
             this.array = new Integer[maxSize];
@@ -64,14 +69,16 @@ public class IntQueueTest {
 
         /**
          * 判断队列是否满了
+         *
          * @return true 满
          */
         public boolean isFull() {
-            return rear==maxSize-1;
+            return (rear+1)%maxSize == front;
         }
 
         /**
          * 判断队列是否空的
+         *
          * @return true 空
          */
         public boolean isEmpty() {
@@ -80,17 +87,20 @@ public class IntQueueTest {
 
         /**
          * 添加数组
+         *
          * @param value
          */
         public void add(Integer value) {
             if (isFull()) {
-                throw new IllegalArgumentException("队列满了:"+value+"丢弃");
+                throw new IllegalArgumentException("队列满了:   " + value + "   丢弃");
             }
-            array[++rear] = value;
+            array[rear] = value;
+            rear = (rear + 1) % maxSize;
         }
 
         /**
          * 从队列中取出一个数据
+         *
          * @return 队列中的值
          */
         public Integer get() {
@@ -98,23 +108,35 @@ public class IntQueueTest {
                 System.out.println("没东西拿");
                 return null;
             }
-            return array[++front];
+            Integer value = array[front];
+            front = (front + 1) % maxSize;
+            return value;
         }
+
         public Integer showHead() {
             if (isEmpty()) {
                 System.out.println("没东西");
                 return null;
             }
-            return array[front+1];
+            return array[front + 1];
         }
 
-
-        @Override
-        public String toString() {
-            return "IntQueue{" +
-                     Arrays.toString(array) +
-                    '}';
+        public int size() {
+            return (rear+maxSize-front)%maxSize;
         }
+
+        public void show() {
+            if (isEmpty()) {
+                System.out.println("队列没东西");
+                return;
+            }
+            StringBuilder sb = new StringBuilder("queue:");
+            for (int i = front ; i < front+size(); i++) {
+                sb.append(array[i%maxSize]).append(",");
+            }
+            System.out.println(sb.toString());
+        }
+
     }
 }
 
